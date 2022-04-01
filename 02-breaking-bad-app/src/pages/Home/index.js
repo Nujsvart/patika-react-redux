427781d6176721f2282df2da481c58d8e87bf442
Nujsvart <kampfar@gmail.com>
2022-04-01 04:06:08 +0300

    karakterlerin listelenmesi - masonry css

--- 02-breaking-bad-app/src/pages/Home/index.js
+++ 02-breaking-bad-app/src/pages/Home/index.js
@@ -1,18 +1,53 @@
 import React, { useEffect } from "react";
+import Masonry from "react-masonry-css";
+import "./styles.css";
+
+import Loading from "../../components/Loading";
+import Error from "../../components/Error";
 
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../../redux/charactersSlice";
 
 const Home = () => {
-  const data = useSelector(state => state.characters);
+  const characters = useSelector(state => state.characters.items);
+  const isLoading = useSelector(state => state.characters.isLoading);
+  const error = useSelector(state => state.characters.error);
+
   const dispatch = useDispatch();
-  console.log(data);
+  console.log(characters);
 
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
-  return <div>Home</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error message={error} />;
+  }
+
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {characters.map(character => (
+          <div key={character.char_id}>
+            <img
+              src={character.img}
+              alt={character.name}
+              className="character"
+            />
+            <div className="charName">{character.name}</div>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 };
 
 export default Home;
